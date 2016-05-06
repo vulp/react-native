b2b59096a207edc673ad5aab2d4f6d3cd13e560f
vulp <tuomo.pohjola@gmail.com>
2016-05-06 21:48:59 +0300

    cleaning

--- index.android.js
+++ index.android.js
@@ -91,16 +91,6 @@ const styles = StyleSheet.create({
   rightContainer: {
     flex: 1,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
   thumbnail: {
     width: 53,
     height: 81,
